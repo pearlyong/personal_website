7fec8c4e9bae3b03a850f2fd9c0ac5e6481cfe3e
Pearly <ong.pearly@gmail.com>
2017-06-24 14:06:34 +0000

    Created Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#goodbye'
+  get 'welcome/index'
+
+  resources :articles
+
+  root 'welcome#index'
 end
