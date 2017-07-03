6b6974f210412d31e8a02fe43b9ea4ad6ed47630
Pearly <ong.pearly@gmail.com>
2017-07-03 08:58:59 +0000

    amend production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,17 +90,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :domain               => "powerful-plains-76550.herokuapp.com"
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto  => true
-  }
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "powerful-plains-76550.herokuapp.com",
+  user_name: ENV['gmail_username'],
+  password: ENV['gmail_password'],
+  authentication: :plain,
+  enable_starttls_auto: true
+}
   
-  config.action_mailer.default_url_options = { :host => 'https://powerful-plains-76550.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'powerful-plains-76550.herokuapp.com' }
   
   config.action_mailer.perform_deliveries = true
   
